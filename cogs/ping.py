1cbae3fe59eb770a8fa66385642de48ad4c02612
Finlay <finjsawyer@gmail.com>
2021-01-10 15:35:16 +0000

    Add http command

--- cogs/ping.py
+++ cogs/ping.py
@@ -1,5 +1,6 @@
 import asyncio
 
+import aiohttp
 from discord.ext import commands
 from ping3 import ping
 
@@ -8,7 +9,7 @@ class Ping(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(brief="Pings an address - ping <address> [pings]")
+    @commands.command(brief="Pings an address", usage="ping <address> [pings]")
     async def ping(self, ctx, address: str, pings: int = 1) -> None:
         """
         Pings an address once or multiple times
@@ -27,6 +28,21 @@ class Ping(commands.Cog):
                     f"Received response from {address} in: {str(int(ping(address, unit='ms')))}ms."
                 )
                 await asyncio.sleep(1)
+
+    @commands.command(brief="Performs a HTTP request", usage="http <address>")
+    async def http(self, ctx, address: str) -> None:
+        """
+        Performs a HTTP request to the specified address
+        :param ctx: commands.Context
+        :param address: Address to make request to
+        :return: HTTP status code
+        """
+        if not address.startswith('http'):
+            address = f'http://{address}'
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(address) as res:
+                await ctx.send(f"Recieved response code: {res.status} ({res.reason})")
 
 
 def setup(bot):
