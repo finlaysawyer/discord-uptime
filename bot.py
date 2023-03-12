a468812e2896b45af97ef0da2d1b4644d35aeaf1
Finlay Sawyer <18363677+finlaysawyer@users.noreply.github.com>
2023-03-12 10:21:23 +0000

    Simplify cog loading

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import asyncio
 import logging
-import os
 import sys
 
 import discord
@@ -10,15 +9,22 @@ from discord.ext import commands
 
 from utils.config import get_config
 
-intents = discord.Intents.default()
-intents.message_content = True
-
 logging.basicConfig(
     format="%(levelname)s | %(asctime)s | %(name)s | %(message)s",
     stream=sys.stdout,
     level=logging.INFO,
 )
 logger = logging.getLogger(__name__)
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+COGS = (
+    "ping",
+    "tcp",
+    "http",
+    # "monitor",
+)
 
 
 class DiscordUptime(commands.Bot):
@@ -37,10 +43,9 @@ class DiscordUptime(commands.Bot):
             else None,
         )
 
-    async def setup_hook(self):
-        for filename in os.listdir("./cogs"):
-            if filename.endswith(".py") and not filename.startswith("__"):
-                await self.load_extension(f"cogs.{filename[:-3]}")
+    async def setup_hook(self) -> None:
+        for cog in COGS:
+            await self.load_extension(f"cogs.{cog}")
 
         guild = discord.Object(id=1023142294905421834)
         self.tree.copy_global_to(guild=guild)
